b55b497598b008ecc682f8b4102c14f8a74a159b
luisgklaynes <luisguilherme.laynes@gmail.com>
2023-11-14 14:14:37 -0300

    Adicionado a coluna "Requisicao"

--- dbo/Tables/tb_teste.sql
+++ dbo/Tables/tb_teste.sql
@@ -2,5 +2,7 @@ CREATE TABLE [dbo].[teste]
 (
     IdTeste int IDENTITY(1,1) NOT NULL,
     Descricao VARCHAR(50) NULL,
-    DataCriacao DATETIME DEFAULT GETDATE() NULL
+    Requisicao VARCHAR(25) NULL,
+    DataCriacao DATETIME DEFAULT GETDATE() NULL,
+
 )
